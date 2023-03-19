cfe9915a7ab07a694c7bf9395054682a2fd5b518
Akshay Patil <iamakshaypatil@gmail.com>
2023-03-19 16:09:30 +0000

    create a math module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const add = require("./math.js");
+
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-let sum = num1 + num2;
+let sum = add(num1, num2);
 console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 console.log(`adding the sum yo and some changes`);
